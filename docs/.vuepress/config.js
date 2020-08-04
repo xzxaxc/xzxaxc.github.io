7ebc4040e13c7a5ec08fc0f958a031ae9a97a559
xzxaxc <69107550+xzxaxc@users.noreply.github.com>
2020-08-04 23:32:57 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: 'Mediumish',
-  base: '/vuepress-theme-mediumish/',
+  base: '/xzxaxc.github.io/',
   description: 'Blog example for Mediumish theme with VuePress',
   logo: './assets/img/logo.png',
   theme: require.resolve('../../'),
