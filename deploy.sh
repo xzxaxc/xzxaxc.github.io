eaaa198e1831bd961e31ce6c198c4098e5be1e7e
xzxaxc <69107550+xzxaxc@users.noreply.github.com>
2020-08-04 23:34:37 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:wowthemesnet/vuepress-theme-mediumish.git master:gh-pages
+git push -f git@github.com:xzxaxc/zxaxc.github.io.git master:gh-pages
 
 cd -
